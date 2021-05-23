c3612da326d19a12e604556b588d199c6b102acf
Cat-Denton <willwdenton@gmail.com>
2021-05-23 14:21:56 -0700

    update to use many-to-many relationships and implement database migration
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>
    Co-authored-by: Collin Chapman <cchap14@gmail.com>

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -32,10 +32,12 @@ namespace ToDoList.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-
     public ActionResult Details(int id)
     {
-      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      Category thisCategory = _db.Categories
+        .Include(category => category.JoinEntities)
+        .ThenInclude(join => join.Item)
+        .FirstOrDefault(category => category.CategoryId == id);
       return View(thisCategory);
     }
 
@@ -70,58 +72,3 @@ namespace ToDoList.Controllers
   }
 }
 
-// namespace ToDoList.Controllers
-// {
-//   public class CategoriesController : Controller
-//   {
-
-//     [HttpGet("/categories")]
-//     public ActionResult Index()
-//     {
-//       List<Category> allCategories = Category.GetAll();
-//       return View(allCategories);
-//     }
-
-//     [HttpGet("/categories/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
-
-//     [HttpPost("/categories")]
-//     public ActionResult Create(string categoryName)
-//     {
-//       Category newCategory = new Category(categoryName);
-//       return RedirectToAction("Index");
-//     }
-
-//     [HttpGet("/categories/{id}")]
-//     public ActionResult Show(int id)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category selectedCategory = Category.Find(id);
-//       List<Item> categoryItems = selectedCategory.Items;
-//       model.Add("category", selectedCategory);
-//       model.Add("items", categoryItems);
-//       return View(model);
-//     }
-
-
-//     // This one creates new Items within a given Category, not new Categories:
-
-//     [HttpPost("/categories/{categoryId}/items")]
-//     public ActionResult Create(int categoryId, string itemDescription)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category foundCategory = Category.Find(categoryId);
-//       Item newItem = new Item(itemDescription);
-//       newItem.Save();
-//       foundCategory.AddItem(newItem);
-//       List<Item> categoryItems = foundCategory.Items;
-//       model.Add("items", categoryItems);
-//       model.Add("category", foundCategory);
-//       return View("Show", model);
-//     }
-
-//   }
-// }
